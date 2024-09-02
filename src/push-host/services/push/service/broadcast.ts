025cee125659ec65f018604719898945f5aabedc
徐超越 <993921@qq.com>
2024-09-02 23:05:13 +0800

    support xmtp/broadcast

--- src/push-host/services/push/service/broadcast.ts
+++ src/push-host/services/push/service/broadcast.ts
@@ -1,5 +1,17 @@
+import { BroadcastClient } from "@xmtp/broadcast-sdk";
 import { BroadcastRequest } from "../model/broadcast";
+import { Client } from "@xmtp/xmtp-js";
+import { Wallet } from "ethers";
+
+const signer = Wallet.createRandom();
 
 export const Broadcast = async (body: BroadcastRequest): Promise<string> => {
+  const client = await Client.create(signer);
+  const broadcastClient = new BroadcastClient({
+    client,
+    addresses: ["0x1234", "0x5678"],
+    cachedCanMessageAddresses: ["0x1234"],
+  })
+  broadcastClient.broadcast(['Hello!'], {})
   return "Broadcasting message...";
 };
